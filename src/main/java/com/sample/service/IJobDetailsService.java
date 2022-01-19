9810b0d6cc8bf60d6415eedbf1daa4298bd7c6cc
selviselvam <selvicse04@gmail.com>
2022-01-19 19:10:47 +0530

    checkin files

---
+++ src/main/java/com/sample/service/IJobDetailsService.java
@@ -0,0 +1,7 @@
+package com.sample.service;
+
+import com.sample.entity.JobDetails;
+
+public interface IJobDetailsService {
+	Integer saveJobDetails(JobDetails jobDetails);
+}
---
+++ src/main/java/com/sample/service/ImpJobDetailsService.java
@@ -0,0 +1,20 @@
+package com.sample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sample.entity.JobDetails;
+import com.sample.repository.JobDetailsRepository;
+@Service
+public class ImpJobDetailsService implements IJobDetailsService {
+	
+	@Autowired
+	   JobDetailsRepository jobDetailsRepository;
+	 
+		@Override
+		public Integer saveJobDetails(JobDetails jobDetails) {
+			int jobDetailId = jobDetailsRepository.save(jobDetails).getJobDetailId();
+			Integer iInteger = Integer.valueOf(jobDetailId);
+			return iInteger;
+		}
+}
