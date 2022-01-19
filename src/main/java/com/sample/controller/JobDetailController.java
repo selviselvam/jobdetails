3525b0d6e6054a504ca3f59d6c113d7532cb67a8
selviselvam <selvicse04@gmail.com>
2022-01-19 19:11:19 +0530

    checkin files

---
+++ src/main/java/com/sample/controller/JobDetailController.java
@@ -0,0 +1,30 @@
+package com.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.entity.JobDetails;
+import com.sample.service.IJobDetailsService;
+
+@RestController
+@RequestMapping("/jobDetails")
+public class JobDetailController {
+	
+ @Autowired 
+ IJobDetailsService jobDetailService;
+ 
+ @RequestMapping(value = "/checkConnection",method = RequestMethod.GET)
+ public String checkConnection() {
+	return "Connection is alive on Job Detail Service"; 
+ }
+ 
+ @RequestMapping(value = "/addJobDetails",method = RequestMethod.POST)
+ public ResponseEntity<String> addJobDetails(@RequestBody JobDetails jobDetails){
+	 Integer jobDetailId = jobDetailService.saveJobDetails(jobDetails);
+	 return ResponseEntity.ok("Job details saved :"+jobDetailId);
+ }
+}
