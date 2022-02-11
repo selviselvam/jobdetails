8d88b8e26f642f87aa6a6d8016bdaf2a8287baba
selviselvam <selvicse04@gmail.com>
2022-02-11 14:33:31 +0530

    adding change

--- src/main/java/com/sample/controller/JobDetailController.java
+++ src/main/java/com/sample/controller/JobDetailController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sample.entity.JobDetails;
@@ -17,6 +18,8 @@ public class JobDetailController {
  @Autowired 
  IJobDetailsService jobDetailService;
  
+ 
+
  @RequestMapping(value = "/checkConnection",method = RequestMethod.GET)
  public String checkConnection() {
 	return "Connection is alive on Job Detail Service"; 
@@ -27,4 +30,10 @@ public class JobDetailController {
 	 Integer jobDetailId = jobDetailService.saveJobDetails(jobDetails);
 	 return ResponseEntity.ok("Job details saved :"+jobDetailId);
  }
+ @RequestMapping(value ="/findJobDetails",method = RequestMethod.GET)
+ public ResponseEntity<JobDetails> findJobDetails(@RequestParam("jobDetailId") int jobDetailId){
+	 JobDetails jobDetails = jobDetailService.findByJobDetailId(jobDetailId);
+	 return ResponseEntity.ok(jobDetails);
+ }
+ 
 }
